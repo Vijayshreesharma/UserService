c75229ed67922b1bd90ec9814e1cc4bb134307ad
Vijayshreesharma <vijayshreesharma2003@gmail.com>
2025-05-02 12:20:35 +0530

    Update Homecontroller.java

--- src/main/java/com/example/javaseeker/Homecontroller.java
+++ src/main/java/com/example/javaseeker/Homecontroller.java
@@ -3,7 +3,12 @@ package com.example.javaseeker;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +16,23 @@ public class Homecontroller {
 	
 	@Autowired
 	UserRepository userRepository;
+	
+	@DeleteMapping("users")
+	public String deleteUser(@RequestBody User user) {
+	 userRepository.delete(user);
+	 return "user deleted";
+	}
+	
+	@PutMapping("users")
+	public User updateUser(@RequestBody User user) {
+	return userRepository.save(user);
+	}
+	
+	
+	@PostMapping("users")
+	public User saveUser(@RequestBody User user) {
+	return userRepository.save(user);
+	}
 	
 	@GetMapping("myname")
 	public String MyName(String name) {
